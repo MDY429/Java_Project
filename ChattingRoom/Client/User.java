bd3c33774bd935284769290a4abaf10c77df429e
TA-YU Mar <txm986@cs.bham.ac.uk>
2020-03-06 02:18:02 +0000

    Add the new class for user and user manager.

---
+++ ChattingRoom/Client/User.java
@@ -0,0 +1,54 @@
+/**
+ * The user class define the user's relative inforation.
+ * @author Ta-Yu Mar
+ * @version 0.1 beta 2020-03-05
+ */
+public class User {
+
+    public String username;
+    public String password;    
+    private DataHandler dataHandler = null;
+
+    /**
+     * Contructor for user.
+     */
+    public User() {
+        
+    }
+
+    /**
+     * Contructor for user.
+     * @param username
+     * @param password
+     */
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    /**
+     * Get the correspondinf data handler.
+     * @return DataHandler
+     */
+    public DataHandler getDataHandler(){
+        return dataHandler;
+    }
+
+    /**
+     * Set the data handler.
+     * @param dataHandler The input of dataHandler.
+     */
+    public void setDataHandler(DataHandler dataHandler){
+        this.dataHandler = dataHandler;
+    }
+
+    /**
+     * Send the DataPackage to server.
+     * @param pkg The input of data package.
+     * @return boolean
+     */
+    public boolean sendDataPackage(DataPackage pkg) {
+        return  dataHandler.sendDataHandle(pkg.toString());
+    }
+
+}
---
+++ ChattingRoom/Client/UserManager.java
@@ -0,0 +1,36 @@
+/**
+ * The UserManager is middle role of client and server.
+ * @author Ta-Yu Mar
+ * @version 0.1 beta 2020-03-05
+ */
+public class UserManager {
+
+    /**
+     * Server handle user sign up new account.
+     * @param pkg The input of data package.
+     * @param dataHandler The input of correspondinf datahandler.
+     */
+    public void userSignUp(DataPackage pkg, DataHandler dataHandler){
+        boolean checkdatabase = true;
+        // TODO: Here will connect Database and check other details.
+        if(checkdatabase){
+            System.out.println("UserManager - signUp");
+            User user = new User(pkg.username, pkg.userpw);
+
+            // Get correspond data handler.
+            user.setDataHandler(dataHandler);
+
+            pkg.flag = 1;
+            
+            // Send back to Client and return scuess.
+            user.getDataHandler().sendDataHandle(pkg.toString());
+        }
+        else {
+            System.out.println("User sign up Error!!!");
+            pkg.flag = 0;
+            
+            dataHandler.sendDataHandle(pkg.toString());
+            return;
+        }
+    }
+}
