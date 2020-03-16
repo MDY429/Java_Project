c667ba23f89d61bfaa1490899b5bca97e841a331
TA-YU Mar <txm986@cs.bham.ac.uk>
2020-03-16 20:48:54 +0000

    Move to correct folder.

--- ChattingRoom/Database/DatabaseManager.java
+++ ChattingRoom/Database/DatabaseManager.java
@@ -7,41 +7,23 @@ import java.sql.*;
  **/
 
 public class DatabaseManager {
-    private final String URL="jdbc:postgresql://mod-msc-sw1.cs.bham.ac.uk:5432/chicago";
-    private final String Driver="org.postgresql.Driver";
-    private final String UserName="chicago";
-    private final String Password="vcm5e28kr0";
-    private Connection connect=null;
+    Database_Conntect dc=new Database_Conntect();
+    // Database_Conntect ds=new Database_Conntect();
 
+    private Connection connect=null;
     //test whether table exist
     public DatabaseManager(){
         if(!table_Exist()){
             add_Table();
         }
-        DB_connect();
+        connect=dc.DB_conntect();
     }
+
 
     private boolean table_Exist(){
         return true;
     }
 
-    //method for test database connect
-    public boolean DB_connect(){
-        try {
-            Class.forName(Driver);
-            connect= DriverManager.getConnection(URL,UserName,Password);
-            System.out.println("connect success");
-            return true;
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            System.out.println("Driver is wrong");
-            return false;
-        }catch (SQLException e){
-            e.printStackTrace();
-            System.out.println("connect fail");
-            return false;
-        }
-    }
     //method for adding table
     public boolean add_Table(){
         if(connect==null){
@@ -51,6 +33,10 @@ public class DatabaseManager {
             Statement statement_createTable=connect.createStatement();
             String postgre_friendlist="create table UserList(ID serial primary key,username  varchar(20) not null unique,password varchar(20) not null,email varchar(20) not null)";
             statement_createTable.executeUpdate(postgre_friendlist);
+
+            Statement statement_friends=connect.createStatement();
+            String user_friendlist="create table User_friendlist(User_id integer not null,username varchar(20) not null,Friend_id integer not null,Friend_username varchar(20) not null)";
+            statement_friends.executeUpdate(user_friendlist);
 
             String postgre_history="create table history(ID integer not null,ID_friend integer not null,content varchar(200),time_history TIMESTAMP(14) default current_timestamp)";
             statement_createTable.executeUpdate(postgre_history);
@@ -64,23 +50,47 @@ public class DatabaseManager {
     }
 
     //insert user
-    public boolean add_users(String name,String password,String email){
-        if(connect==null){
+    public  boolean add_users(String name,String password,String email) {
+        if (connect == null) {
             return false;
         }
-        try{
-            PreparedStatement insert_user=connect.prepareStatement("insert into UserList(username,password,email) values(?,?,?)");
-            insert_user.setString(1,name);
-            insert_user.setString(2,password);
-            insert_user.setString(3,email);
-            int num=insert_user.executeUpdate();
-            System.out.println("add successfully");
-            return num>0;
-        }catch (SQLException e) {
-            System.out.println("mistake in adding user");
+        if (password.indexOf(" ") != -1) {
+            System.out.println("passwords are not allowed to have blank");
             return false;
+        }
+        if (password.length() < 5 || name.length() < 5) {
+            System.out.println("you should set longer passwords or username");
+           return false;
+        }
+        if (!isValidword(name)) {
+            System.out.println("illegal usename");
+            return false;
+        }
+        if (!email.contains("@")) {
+            System.out.println("illegal email format ");
+            return false;
+        }
+            else{
+            try {
+                PreparedStatement insert_user = connect.prepareStatement("insert into UserList(username,password,email) values(?,?,?)");
+                insert_user.setString(1, name);
+                insert_user.setString(2, password);
+                insert_user.setString(3, email);
+                int num = insert_user.executeUpdate();
+                System.out.println("add successfully");
+                return num > 0;
+            } catch (SQLException e) {
+                System.out.println("mistake in adding user");
+                return false;
+            }
         }
     }
+    //isValidWord
+    public boolean isValidword(String word){
+        String regex="^[a-zA-Z0-9]+$";
+        return word.matches(regex);
+    }
+
     //search user
     public int search_user(String userName,String password){
         int id_user=-1;
@@ -102,12 +112,9 @@ public class DatabaseManager {
         return -1;
     }
 
+
     //add history
     public boolean add_history(Integer id, Integer ID_friend,String content){
-//        java.util.Date date1 = new java.util.Date();
-//        long time2=date1.getTime();
-//        Timestamp timestamp=new Timestamp(time2);
-//        Date date=new Date(time2);
         if(connect==null){
             return false;
         }
@@ -191,18 +198,69 @@ public class DatabaseManager {
         }
     }
 
+    //search user for add friends
+    public int search_user_addfriends(String userName){
+        int id_user=-1;
+        if(connect==null){
+            System.out.println("database do not connect");
+        }
+        try{
+            PreparedStatement search=connect.prepareStatement("select id from UserList where username=?");
+            search.setString(1,userName);
+            ResultSet resultSet=search.executeQuery();
+            while(resultSet.next()){
+                id_user=resultSet.getInt(1);
+            }
+            return id_user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    //add friends to friends list
+    public boolean add_friends(Integer id, String user_username,Integer ID_friend,String friends_username){
+        if(connect==null){
+            return false;
+        }
+        if(ID_friend<0){
+            System.out.println("add friends unsuccessfully");
+            return false;
+        }
+        try{
+            PreparedStatement insert_content=connect.prepareStatement("insert into User_friendlist(User_id,username,Friend_id,Friend_username) values(?,?,?,?)");
+            insert_content.setInt(1,id);
+            insert_content.setString(2,user_username);
+            insert_content.setInt(3,ID_friend);
+            insert_content.setString(4,friends_username);
+            int num=insert_content.executeUpdate();
+            System.out.println("add friends successfully");
+            return num>0;
+        } catch (SQLException e) {
+            System.out.println("add friends unsuccessfully");
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
     public static void main(String[] args) {
         DatabaseManager db = new DatabaseManager();
+        db.add_friends(1,"aaaa",-1,"bbbb");
+     //   db.add_users("bbbbbb","asasda","asdasd@");
+//        System.out.println(db.search_user_addfriends("bbbbbb"));
+       // db.add_users("ifu","asd","asdasd");
+       // db.add_users("+++++++","asd","asdasd");
         //db.add_users("2","2","333");//success
            //db.add_Table();//success
-//            db.add_history(1,2,"a");//success
+           // db.add_history(1,2,"a");//success
 //            db.add_history(1,2,"abc");//success
 //            db.add_history(1,2,"bac");//success
 //            db.add_history(2,2,"cba");//success
 //            db.add_history(3,2,"a");//success
         // db.delete_history(2,"abc");//success
         // db.delete_user(2);//success
-        System.out.println(db.search_user("5","2"));
+        //System.out.println(db.search_user("5","2"));
 
          //db.search_history(1,2,"a");
     }
