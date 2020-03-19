34cec4df6109ca9adfa37c952455bf9cdb3c50c1
Yongnan Zhang <yxz1017@cs.bham.ac.uk>
2020-03-19 12:46:27 +0000

    Replace Database_Conntect.java

--- ChattingRoom/Database/Database_Conntect.java
+++ ChattingRoom/Database/Database_Conntect.java
@@ -1,11 +1,6 @@
-import java.io.FileNotFoundException;
 import java.sql.*;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 /**
  * @author ynz
@@ -13,26 +8,26 @@ import java.util.Properties;
  * @description:this is the class for
  **/
 
-public class Database_Conntect {
+public class Database_Connect {
     private final String URL = "jdbc:postgresql://mod-msc-sw1.cs.bham.ac.uk:5432/chicago?serverTimezone=UTC&amp;characterEncoding=utf-8";
     private final String Driver = "org.postgresql.Driver";
     private final String UserName = "chicago";
     private final String Password = "vcm5e28kr0";
-    private Connection conntect = null;
+    private Connection connect = null;
 
-    public Connection DB_conntect() {
+    public Connection DB_connect() {
         try {
             Class.forName(Driver);
-            conntect = DriverManager.getConnection(URL, UserName, Password);
-            System.out.println("conntect success");
-            return conntect;
+            connect = DriverManager.getConnection(URL, UserName, Password);
+            System.out.println("connect success");
+            return connect;
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
             System.out.println("Driver is wrong");
             return null;
         } catch (SQLException e) {
             e.printStackTrace();
-            System.out.println("conntect fail");
+            System.out.println("connect fail");
             return null;
         }
     }
